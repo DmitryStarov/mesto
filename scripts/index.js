69fbaae7329191912b13da98e8c46963bd3a2862
DmitryStarov <314263@bk.ru>
2023-02-05 00:34:23 +0800

    JS popupSaveButton

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,11 @@
 const editButton = document.querySelector('.profile__button-edit');
 const popup = document.querySelector('.popup');
-
 let userName = document.querySelector('.profile__name');
 let userAbout = document.querySelector('.profile__about');
-
 let popupForm = document.querySelector('.popup__container');
-const popupSaveButton = popup.querySelector('.popup__button-save')
 let popupInputName = popupForm.querySelector('.popup__input_type_name');
 let popupInputAbout = popupForm.querySelector('.popup__input_type_about');
+
 
 const handleOverlyClick = (event) => {
   if (event.target === event.currentTarget) {
@@ -15,11 +13,16 @@ const handleOverlyClick = (event) => {
   }
 };
 
-const handleFormSubmit = (event) => {                 //отправляем форму
+const handleCloseButtonClick = () => {
+  toggleOpenPopup();
+};
+
+
+const handleFormSubmit = (event) => {
   event.preventDefault();
   userName.textContent = popupInputName.value;
   userAbout.textContent = popupInputAbout.value;
-  popup.classList.toggle("popup_opened");             //закрываем попап
+  popup.classList.toggle("popup_opened");
 }
 
 const toggleOpenPopup = () =>{
@@ -27,9 +30,10 @@ const toggleOpenPopup = () =>{
   popupInputAbout.value = userAbout.innerText;
   popup.classList.toggle("popup_opened");
   popup.addEventListener("click", handleOverlyClick);
-  popupSaveButton.addEventListener("click", popupForm.addEventListener("submit", handleFormSubmit));
 }
 
 
 
 editButton.addEventListener("click", toggleOpenPopup);
+popupForm.addEventListener("submit", handleFormSubmit);
+popupCloseButton.addEventListener("click", handleCloseButtonClick );
