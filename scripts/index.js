28238c075dc141778ccf31196df84f3c9c7c4e5a
DmitryStarov <314263@bk.ru>
2023-02-06 18:56:36 +0800

    bugfix/Review 1

--- scripts/index.js
+++ scripts/index.js
@@ -2,35 +2,34 @@ const editButton = document.querySelector('.profile__button-edit');
 const popup = document.querySelector('.popup');
 let userName = document.querySelector('.profile__name');
 let userAbout = document.querySelector('.profile__about');
-let popupForm = document.querySelector('.popup__container');
+let popupForm = document.querySelector('.popup__form');
 let popupInputName = popupForm.querySelector('.popup__input_type_name');
 let popupInputAbout = popupForm.querySelector('.popup__input_type_about');
 const popupCloseButton = document.querySelector(".popup__button-close");
 
-const handleOverlyClick = (event) => {
-  if (event.target === event.currentTarget) {
-    toggleOpenPopup();
-  }
-};
 
-const handleCloseButtonClick = () => {
-  toggleOpenPopup();
-};
+const openPopup = () => {
+  popup.classList.add('popup_opened');
+}
+
+const closePopup = () => {
+  popup.classList.remove("popup_opened");
+}
+
+const openEditPopup = () =>{
+  popupInputName.value = userName.innerText;
+  popupInputAbout.value = userAbout.innerText;
+  openPopup();
+}
 
 const handleFormSubmit = (event) => {
   event.preventDefault();
   userName.textContent = popupInputName.value;
   userAbout.textContent = popupInputAbout.value;
-  popup.classList.toggle("popup_opened");
+  closePopup();
 }
 
-const toggleOpenPopup = () =>{
-  popupInputName.value = userName.innerText;
-  popupInputAbout.value = userAbout.innerText;
-  popup.classList.toggle("popup_opened");
-  popup.addEventListener("click", handleOverlyClick);
-}
 
-editButton.addEventListener("click", toggleOpenPopup);
+editButton.addEventListener("click", openEditPopup);
 popupForm.addEventListener("submit", handleFormSubmit);
-popupCloseButton.addEventListener("click", handleCloseButtonClick );
+popupCloseButton.addEventListener("click", closePopup);
