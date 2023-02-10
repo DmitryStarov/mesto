20657bf766d0d8876f49340559bba02d026815e3
DmitryStarov <314263@bk.ru>
2023-02-10 14:42:24 +0800

    feature/like action

--- scripts/index.js
+++ scripts/index.js
@@ -42,6 +42,9 @@ const createCard = (item) => {
   const cardElement = cardTemplate.querySelector('.cards__item').cloneNode(true);
   cardElement.querySelector('.cards__title').textContent = item.name;
   cardElement.querySelector('.cards__image').src = item.link;
+  cardElement.querySelector('.cards__button-like').addEventListener('click', (evt) => {
+    evt.target.classList.toggle('cards__button-like_action');
+  })
   console.log(cardElement);
   return cardElement;
 }
