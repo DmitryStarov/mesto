e78f8e34f4a317abc6b11c1c0c7d1fc5521d5913
DmitryStarov <314263@bk.ru>
2023-02-10 12:13:14 +0800

    feature/create cards, render cards

--- scripts/index.js
+++ scripts/index.js
@@ -8,6 +8,54 @@ let popupInputAbout = popupForm.querySelector('.popup__input_type_about');
 const popupCloseButton = document.querySelector('.popup__button-close');
 
 
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const createCard = (item) => {
+  const cardTemplate = document.querySelector('#card-template').content;
+  console.log(cardTemplate);
+  const cardElement = cardTemplate.querySelector('.cards__item').cloneNode(true);
+  cardElement.querySelector('.cards__title').textContent = item.name;
+  cardElement.querySelector('.cards__image').src = item.link;
+  console.log(cardElement);
+  return cardElement;
+}
+
+const renderCard = (item) => {
+  const newCard = createCard(item);
+  const cardContainer = document.querySelector('.cards');
+  cardContainer.prepend(newCard);
+}
+
+initialCards.forEach((item) => {
+  renderCard(item);
+})
+
 const openPopup = () => {
   popup.classList.add('popup_opened');
 }
@@ -33,3 +81,4 @@ const handleFormSubmit = (event) => {
 editButton.addEventListener('click', openEditPopup);
 popupForm.addEventListener('submit', handleFormSubmit);
 popupCloseButton.addEventListener('click', closePopup);
+
