b58c422e3a7f16affcd63920dabb401875cbb955
DmitryStarov <314263@bk.ru>
2023-04-25 01:27:17 +0800

    refactoring constant.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -26,33 +26,6 @@ const validationSource = {
   errorMessageSelector: 'popup__error-message_visible'
 };
 
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
-
 const formValidators = {};
 
 export {
@@ -67,7 +40,6 @@ export {
   cardsContainerSelector,
   cardTemplate,
   validationSource,
-  initialCards,
   formValidators,
   userInfo,
   userAvatarEdit
